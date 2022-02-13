0cf99900f87dd35974ae1a98ede9b25b78d7fd25
ProfBytes <snyde479@umn.edu>
2022-02-13 11:46:28 -0600

    Initial commit

---
+++ src/generator/BingoGenerator.java
@@ -0,0 +1,24 @@
+package generator;
+
+public class BingoGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+/**
+ * Catch {rare pokemon}
+Catch {number} {type} type pokemon
+Find {rare item from time distortion}
+Catch alpha {non-rare pokemon}
+Find a pokemon with {move} 
+Evolve {number} pokemon
+Dig{rare item up}
+Craft {number} {item}
+Catch a shiny
+Stun {pokemon or number of poekemon}
+spell {word}
+fight {character}
+ */
